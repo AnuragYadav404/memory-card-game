b60754804a9728b0d61794f3230a764e76058c1a
starboy <124185505+new-Begginings@users.noreply.github.com>
2023-08-21 17:51:20 +0530

    Updates styling of pokemon cards

--- src/App.jsx
+++ src/App.jsx
@@ -47,8 +47,9 @@ function App() {
 
   return (
     <div className="container">
-      <h2>Score: {score.current}</h2>
-      <h2>Best Score: {best}</h2>
+      <h3>Dont select the same card twice</h3>
+      <h3>Score: {score.current}</h3>
+      <h3>Best Score: {best}</h3>
       {gameOver && (
         <div>
           <h1>GAME OVER!!!</h1>
@@ -65,14 +66,13 @@ function App() {
                 onClick={(e) => handleSelection(e, pokemon.name)}
                 className="card"
               >
-                <h3>Pokemon: {pokemon.name}</h3>
+                <p>{pokemon.name}</p>
                 <img src={pokemon.url} alt={pokemon.name} />
               </div>
             );
           })}
         </div>
       )}
-      <h1>Dont select the same card twice</h1>
     </div>
   );
 }
